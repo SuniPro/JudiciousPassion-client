95a2e3b96c77ea54d8733a060325860fbb2c07d2
SoonHyun <suni@team-mono.com>
2024-12-17 00:11:33 +0900

    feat : user 기능 도입중

---
+++ src/model/User.tsx
@@ -0,0 +1,14 @@
+export interface User {
+  id?: number;
+  email: string;
+  username: string;
+  phoneNumber: string;
+  password: string;
+  roleType?: "admin" | "writer" | "guest";
+  insertDate?: string | null;
+  insertId?: string | null;
+  updateDate?: string | null;
+  updateId?: string | null;
+  deleteDate?: string | null;
+  deleteId?: string | null;
+}
