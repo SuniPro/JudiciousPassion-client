0d1ff49503ffc79a77c82fc38d42eda26bc10394
Suni <thtnsgus21@gmail.com>
2025-01-08 15:37:14 +0900

    feat : ellipsis, palletCircle 등 공통으로 사용되는 layout 추가

--- src/components/Layouts/Layouts.tsx
+++ src/components/Layouts/Layouts.tsx
@@ -2,17 +2,23 @@ import styled from "@emotion/styled";
 import { css } from "@emotion/react";
 import theme from "../../styles/theme";
 
-export const PageContainer = styled.div`
-  width: 630px;
-  display: flex;
-  flex-direction: column;
-  justify-items: flex-start;
-  align-items: center;
+export const PageContainer = styled.div<{ width: number }>(
+  ({ width = 630 }) => css`
+    width: ${width - 10}px;
+    max-width: 630px;
+    display: flex;
+    flex-direction: column;
+    justify-items: flex-start;
+    align-items: center;
 
-  height: 100%;
+    height: 100%;
+    position: relative;
+    padding-bottom: 30px;
 
-  position: relative;
-`;
+    @media ${theme.windowSize.small} {
+    }
+  `,
+);
 
 export const Divider = styled.p<{ direction?: boolean; size?: number }>(
   ({ direction, size = 100 }) => css`
@@ -23,5 +29,52 @@ export const Divider = styled.p<{ direction?: boolean; size?: number }>(
 
 export const ContentsAreaContainer = styled.div`
   width: 100%;
-  height: 90vh;
 `;
+
+export function EllipsisCase(props: {
+  text: string;
+  textAlign: string;
+  className?: string;
+  width?: number;
+  func?: any;
+}) {
+  const { width, text, className, textAlign, func } = props;
+  return (
+    <TextCase className={className} onClick={func}>
+      <TextArea textAlign={textAlign} width={width}>
+        {text}
+      </TextArea>
+    </TextCase>
+  );
+}
+
+export const TextCase = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
+`;
+
+export const TextArea = styled.span<{ width?: number; textAlign: string }>(
+  ({ width, textAlign }) => css`
+    text-align: ${textAlign};
+    white-space: nowrap;
+    text-overflow: ellipsis;
+    display: block;
+    overflow: hidden;
+    width: ${width}px;
+    height: 100%;
+  `,
+);
+
+export const PalletCircle = styled.div<{ backgroundColor: string }>(
+  ({ backgroundColor }) => css`
+    width: 18px;
+    height: 18px;
+    border-radius: ${theme.borderRadius.circle};
+    border: ${theme.colors.white} 3px solid;
+    box-shadow: ${theme.shadowStyle.default};
+    background-color: ${backgroundColor};
+  `,
+);
