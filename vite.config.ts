b5fe5c063b5b1d419d54d4e0d3a43b9ec9ad4def
Suni <thtnsgus21@gmail.com>
2025-01-08 15:43:31 +0900

    feat : 모바일 환경 크로스체크를 위한 서버개방

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,21 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: "0.0.0.0",
     proxy: {
+      // 첫 번째 프록시 설정
       "/api": {
         target: "http://localhost:8080",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
+        secure: false,
+        ws: true,
+      },
+      // 두 번째 프록시 설정
+      "/private": {
+        target: "http://localhost:8090",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/private/, ""),
         secure: false,
         ws: true,
       },
