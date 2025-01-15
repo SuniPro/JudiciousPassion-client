b630dbc7cadf3b90eb83c763365c33d839aabb79
Suni <thtnsgus21@gmail.com>
2025-01-15 21:07:41 +0900

    feat : saunter ICON 추가

--- src/components/FeatherIcon/Icons.tsx
+++ src/components/FeatherIcon/Icons.tsx
@@ -1,6 +1,10 @@
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+
 export function EditIcon(props: { className?: string }) {
   return (
     <svg
+      className={props.className}
       xmlns="http://www.w3.org/2000/svg"
       width="24"
       height="24"
@@ -10,7 +14,6 @@ export function EditIcon(props: { className?: string }) {
       strokeWidth="2"
       strokeLinecap="round"
       strokeLinejoin="round"
-      className="feather feather-edit"
     >
       <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path>
       <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path>
@@ -21,6 +24,7 @@ export function EditIcon(props: { className?: string }) {
 export function HelpIcon(props: { className?: string }) {
   return (
     <svg
+      className={props.className}
       xmlns="http://www.w3.org/2000/svg"
       width="24"
       height="24"
@@ -30,7 +34,6 @@ export function HelpIcon(props: { className?: string }) {
       strokeWidth="2"
       strokeLinecap="round"
       strokeLinejoin="round"
-      className="feather feather-help-circle"
     >
       <circle cx="12" cy="12" r="10"></circle>
       <path d="M9.09 9a3 3 0 0 1 5.83 1c0 2-3 3-3 3"></path>
@@ -42,6 +45,7 @@ export function HelpIcon(props: { className?: string }) {
 export function HomeIcon(props: { className?: string }) {
   return (
     <svg
+      className={props.className}
       xmlns="http://www.w3.org/2000/svg"
       width="24"
       height="24"
@@ -51,7 +55,6 @@ export function HomeIcon(props: { className?: string }) {
       strokeWidth="2"
       strokeLinecap="round"
       strokeLinejoin="round"
-      className="feather feather-home"
     >
       <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
       <polyline points="9 22 9 12 15 12 15 22"></polyline>
@@ -62,6 +65,7 @@ export function HomeIcon(props: { className?: string }) {
 export function LogInIcon(props: { className?: string }) {
   return (
     <svg
+      className={props.className}
       xmlns="http://www.w3.org/2000/svg"
       width="24"
       height="24"
@@ -71,7 +75,6 @@ export function LogInIcon(props: { className?: string }) {
       strokeWidth="2"
       strokeLinecap="round"
       strokeLinejoin="round"
-      className="feather feather-log-in"
     >
       <path d="M15 3h4a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2h-4"></path>
       <polyline points="10 17 15 12 10 7"></polyline>
@@ -83,6 +86,7 @@ export function LogInIcon(props: { className?: string }) {
 export function LogOutIcon(props: { className?: string }) {
   return (
     <svg
+      className={props.className}
       xmlns="http://www.w3.org/2000/svg"
       width="24"
       height="24"
@@ -92,7 +96,6 @@ export function LogOutIcon(props: { className?: string }) {
       strokeWidth="2"
       strokeLinecap="round"
       strokeLinejoin="round"
-      className="feather feather-log-out"
     >
       <path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"></path>
       <polyline points="16 17 21 12 16 7"></polyline>
@@ -104,6 +107,7 @@ export function LogOutIcon(props: { className?: string }) {
 export function MessageIcon(props: { className?: string }) {
   return (
     <svg
+      className={props.className}
       xmlns="http://www.w3.org/2000/svg"
       width="24"
       height="24"
@@ -113,7 +117,6 @@ export function MessageIcon(props: { className?: string }) {
       strokeWidth="2"
       strokeLinecap="round"
       strokeLinejoin="round"
-      className="feather feather-message-circle"
     >
       <path d="M21 11.5a8.38 8.38 0 0 1-.9 3.8 8.5 8.5 0 0 1-7.6 4.7 8.38 8.38 0 0 1-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 0 1-.9-3.8 8.5 8.5 0 0 1 4.7-7.6 8.38 8.38 0 0 1 3.8-.9h.5a8.48 8.48 0 0 1 8 8v.5z"></path>
     </svg>
@@ -123,6 +126,7 @@ export function MessageIcon(props: { className?: string }) {
 export function SettingsIcon(props: { className?: string }) {
   return (
     <svg
+      className={props.className}
       xmlns="http://www.w3.org/2000/svg"
       width="24"
       height="24"
@@ -132,7 +136,6 @@ export function SettingsIcon(props: { className?: string }) {
       strokeWidth="2"
       strokeLinecap="round"
       strokeLinejoin="round"
-      className="feather feather-settings"
     >
       <circle cx="12" cy="12" r="3"></circle>
       <path d="M19.4 15a1.65 1.65 0 0 0 .33 1.82l.06.06a2 2 0 0 1 0 2.83 2 2 0 0 1-2.83 0l-.06-.06a1.65 1.65 0 0 0-1.82-.33 1.65 1.65 0 0 0-1 1.51V21a2 2 0 0 1-2 2 2 2 0 0 1-2-2v-.09A1.65 1.65 0 0 0 9 19.4a1.65 1.65 0 0 0-1.82.33l-.06.06a2 2 0 0 1-2.83 0 2 2 0 0 1 0-2.83l.06-.06a1.65 1.65 0 0 0 .33-1.82 1.65 1.65 0 0 0-1.51-1H3a2 2 0 0 1-2-2 2 2 0 0 1 2-2h.09A1.65 1.65 0 0 0 4.6 9a1.65 1.65 0 0 0-.33-1.82l-.06-.06a2 2 0 0 1 0-2.83 2 2 0 0 1 2.83 0l.06.06a1.65 1.65 0 0 0 1.82.33H9a1.65 1.65 0 0 0 1-1.51V3a2 2 0 0 1 2-2 2 2 0 0 1 2 2v.09a1.65 1.65 0 0 0 1 1.51 1.65 1.65 0 0 0 1.82-.33l.06-.06a2 2 0 0 1 2.83 0 2 2 0 0 1 0 2.83l-.06.06a1.65 1.65 0 0 0-.33 1.82V9a1.65 1.65 0 0 0 1.51 1H21a2 2 0 0 1 2 2 2 2 0 0 1-2 2h-.09a1.65 1.65 0 0 0-1.51 1z"></path>
@@ -143,6 +146,7 @@ export function SettingsIcon(props: { className?: string }) {
 export function UserCheckIcon(props: { className?: string }) {
   return (
     <svg
+      className={props.className}
       xmlns="http://www.w3.org/2000/svg"
       width="24"
       height="24"
@@ -152,7 +156,6 @@ export function UserCheckIcon(props: { className?: string }) {
       strokeWidth="2"
       strokeLinecap="round"
       strokeLinejoin="round"
-      className="feather feather-user-check"
     >
       <path d="M16 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path>
       <circle cx="8.5" cy="7" r="4"></circle>
@@ -164,6 +167,7 @@ export function UserCheckIcon(props: { className?: string }) {
 export function PhoneIcon(props: { className?: string }) {
   return (
     <svg
+      className={props.className}
       xmlns="http://www.w3.org/2000/svg"
       width="24"
       height="24"
@@ -173,7 +177,6 @@ export function PhoneIcon(props: { className?: string }) {
       strokeWidth="2"
       strokeLinecap="round"
       strokeLinejoin="round"
-      className="feather feather-phone"
     >
       <path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z"></path>
     </svg>
@@ -183,6 +186,7 @@ export function PhoneIcon(props: { className?: string }) {
 export function EmailIcon(props: { className?: string }) {
   return (
     <svg
+      className={props.className}
       xmlns="http://www.w3.org/2000/svg"
       width="24"
       height="24"
@@ -192,7 +196,6 @@ export function EmailIcon(props: { className?: string }) {
       strokeWidth="2"
       strokeLinecap="round"
       strokeLinejoin="round"
-      className="feather feather-at-sign"
     >
       <circle cx="12" cy="12" r="4"></circle>
       <path d="M16 8v5a3 3 0 0 0 6 0v-1a10 10 0 1 0-3.92 7.94"></path>
@@ -203,6 +206,7 @@ export function EmailIcon(props: { className?: string }) {
 export function MapPinIcon(props: { className?: string }) {
   return (
     <svg
+      className={props.className}
       xmlns="http://www.w3.org/2000/svg"
       width="24"
       height="24"
@@ -212,7 +216,6 @@ export function MapPinIcon(props: { className?: string }) {
       strokeWidth="2"
       strokeLinecap="round"
       strokeLinejoin="round"
-      className="feather feather-map-pin"
     >
       <path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z"></path>
       <circle cx="12" cy="10" r="3"></circle>
@@ -223,6 +226,7 @@ export function MapPinIcon(props: { className?: string }) {
 export function UserPlusIcon(props: { className?: string }) {
   return (
     <svg
+      className={props.className}
       xmlns="http://www.w3.org/2000/svg"
       width="24"
       height="24"
@@ -232,7 +236,6 @@ export function UserPlusIcon(props: { className?: string }) {
       strokeWidth="2"
       strokeLinecap="round"
       strokeLinejoin="round"
-      className="feather feather-user-plus"
     >
       <path d="M16 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path>
       <circle cx="8.5" cy="7" r="4"></circle>
@@ -245,6 +248,7 @@ export function UserPlusIcon(props: { className?: string }) {
 export function NavigationIcon(props: { className?: string }) {
   return (
     <svg
+      className={props.className}
       xmlns="http://www.w3.org/2000/svg"
       width="24"
       height="24"
@@ -254,7 +258,6 @@ export function NavigationIcon(props: { className?: string }) {
       strokeWidth="2"
       strokeLinecap="round"
       strokeLinejoin="round"
-      className="feather feather-navigation"
     >
       <polygon points="3 11 22 2 13 21 11 13 3 11"></polygon>
     </svg>
@@ -264,6 +267,7 @@ export function NavigationIcon(props: { className?: string }) {
 export function ShareIcon(props: { className?: string }) {
   return (
     <svg
+      className={props.className}
       xmlns="http://www.w3.org/2000/svg"
       width="24"
       height="24"
@@ -273,13 +277,128 @@ export function ShareIcon(props: { className?: string }) {
       strokeWidth="2"
       strokeLinecap="round"
       strokeLinejoin="round"
-      className="feather feather-share-2"
     >
       <circle cx="18" cy="5" r="3"></circle>
       <circle cx="6" cy="12" r="3"></circle>
       <circle cx="18" cy="19" r="3"></circle>
       <line x1="8.59" y1="13.51" x2="15.42" y2="17.49"></line>
       <line x1="15.41" y1="6.51" x2="8.59" y2="10.49"></line>
+    </svg>
+  );
+}
+
+export function WalkIcon(props: { className?: string }) {
+  return (
+    <svg
+      className={props.className}
+      width="800px"
+      height="800px"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M14 22V16.9612C14 16.3537 13.7238 15.7791 13.2494 15.3995L11.5 14M11.5 14L13 7.5M11.5 14L10 13M13 7.5L11 7M13 7.5L15.0426 10.7681C15.3345 11.2352 15.8062 11.5612 16.3463 11.6693L18 12M10 13L11 7M10 13L8 22M11 7L8.10557 8.44721C7.428 8.786 7 9.47852 7 10.2361V12M14.5 3.5C14.5 4.05228 14.0523 4.5 13.5 4.5C12.9477 4.5 12.5 4.05228 12.5 3.5C12.5 2.94772 12.9477 2.5 13.5 2.5C14.0523 2.5 14.5 2.94772 14.5 3.5Z"
+        stroke="#000000"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
+
+export function BicycleIcon(props: { className?: string }) {
+  return (
+    <svg
+      className={props.className}
+      fill="#000000"
+      height="800px"
+      width="800px"
+      version="1.1"
+      id="Capa_1"
+      xmlns="http://www.w3.org/2000/svg"
+      xmlnsXlink="http://www.w3.org/1999/xlink"
+      viewBox="0 0 491.351 491.351"
+      xmlSpace="preserve"
+    >
+      <path
+        id="XMLID_188_"
+        d="M381.559,182.007c-0.895,0-1.755,0.115-2.632,0.138l-14.934-87.322c-0.809-4.71-3.613-8.842-7.674-11.355
+	c-4.051-2.511-8.998-3.154-13.582-1.772l-44.975,13.622c-8.792,2.667-13.763,11.963-11.097,20.771
+	c2.667,8.801,11.992,13.789,20.757,11.111l26.901-8.15l6.167,35.949H198.895v-7.076l17.403-4.393
+	c6.916-1.741,11.845-8.19,11.433-15.575c-0.464-8.369-7.639-14.77-16.018-14.29l-64.285,3.653c-1.282,0.08-2.701,0.292-4.001,0.616
+	c-11.415,2.889-18.349,14.487-15.467,25.906c2.882,11.419,14.495,18.339,25.911,15.458l11.707-2.959v10.815l-13.694,23.586
+	c-12.964-5.61-27.142-8.733-42.032-8.733C49.282,182.007,0,233.227,0,296.184C0,359.151,49.282,410.37,109.852,410.37
+	c55.081,0,100.708-42.453,108.494-97.529h17.763c4.732,0,9.239-2.017,12.396-5.547l24.432-27.287
+	c-0.732,5.304-1.239,10.673-1.239,16.177c0,62.968,49.291,114.187,109.861,114.187c60.543,0,109.792-51.219,109.792-114.187
+	C491.351,233.227,442.102,182.007,381.559,182.007z M109.852,377.058c-42.203,0-76.544-36.28-76.544-80.874
+	c0-44.591,34.341-80.864,76.544-80.864c8.783,0,17.084,1.895,24.956,4.789l-39.365,67.709c-2.994,5.148-3.011,11.508-0.035,16.672
+	c2.959,5.165,8.474,8.352,14.444,8.352h74.824C177.39,349.464,146.62,377.058,109.852,377.058z M184.676,279.528h-45.868
+	l24.078-41.406C173.682,249.151,181.476,263.423,184.676,279.528z M228.66,279.528h-10.314
+	c-4.001-28.383-17.988-53.433-38.255-71.018l11.743-20.202h118.464L228.66,279.528z M381.559,377.058
+	c-42.211,0-76.544-36.28-76.544-80.874c0-33.481,19.355-62.272,46.874-74.536l13.239,77.351c1.403,8.118,8.431,13.842,16.397,13.842
+	c0.945,0,1.892-0.072,2.829-0.236c9.085-1.553,15.166-10.167,13.618-19.232l-13.307-77.72c40.714,1.774,73.378,37.07,73.378,80.531
+	C458.043,340.777,423.719,377.058,381.559,377.058z"
+      />
+    </svg>
+  );
+}
+
+export function CarIcon(props: { className?: string }) {
+  return (
+    <svg
+      className={props.className}
+      width="800px"
+      height="800px"
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M16,6l3,4h2c1.11,0,2,0.89,2,2v3h-2c0,1.66-1.34,3-3,3s-3-1.34-3-3H9c0,1.66-1.34,3-3,3s-3-1.34-3-3H1v-3c0-1.11,0.89-2,2-2
+		l3-4H16 M10.5,7.5H6.75L4.86,10h5.64V7.5 M12,7.5V10h5.14l-1.89-2.5H12 M6,13.5c-0.83,0-1.5,0.67-1.5,1.5s0.67,1.5,1.5,1.5
+		s1.5-0.67,1.5-1.5S6.83,13.5,6,13.5 M18,13.5c-0.83,0-1.5,0.67-1.5,1.5s0.67,1.5,1.5,1.5s1.5-0.67,1.5-1.5S18.83,13.5,18,13.5z"
+      />
+      <rect fill="none" width="24" height="24" />
+    </svg>
+  );
+}
+
+export function BusIcon(props: { className?: string }) {
+  return (
+    <svg
+      className={props.className}
+      width="800px"
+      height="800px"
+      viewBox="0 0 24 24"
+      id="Layer_1"
+      data-name="Layer 1"
+      xmlns="http://www.w3.org/2000/svg"
+      css={css`
+        .cls-1 {
+          fill: none;
+          stroke: #a8a8a8;
+          stroke-miterlimit: 10;
+          stroke-width: 1.91px;
+        }
+      `}
+    >
+      <defs></defs>
+      <circle className="cls-1" cx="6.27" cy="17.73" r="1.91" />
+      <circle className="cls-1" cx="17.73" cy="17.73" r="1.91" />
+      <path
+        className="cls-1"
+        d="M4.36,17.73H1.5V6.27A1.91,1.91,0,0,1,3.41,4.36H20.59A1.91,1.91,0,0,1,22.5,6.27V17.73H19.64"
+      />
+      <line className="cls-1" x1="15.82" y1="17.73" x2="8.18" y2="17.73" />
+      <line className="cls-1" x1="17.73" y1="5.32" x2="17.73" y2="10.09" />
+      <path
+        className="cls-1"
+        d="M22.5,13H19.64a1.9,1.9,0,0,1-1.91-1.9v-1H1.5"
+      />
+      <line className="cls-1" x1="13.91" y1="5.32" x2="13.91" y2="10.09" />
+      <line className="cls-1" x1="6.27" y1="5.32" x2="6.27" y2="10.09" />
+      <line className="cls-1" x1="10.09" y1="5.32" x2="10.09" y2="10.09" />
     </svg>
   );
 }
